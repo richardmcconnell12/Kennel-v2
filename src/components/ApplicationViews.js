52dd18cba7395f4b0c728267d6c24f9192569c42
Ricky McConnell <mr.mcconnell12@yahoo.com>
2020-01-23 14:29:43 -0600

    Set up navbar and routes -- rendering each component accordingly

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,49 @@
+import { Route } from "react-router-dom";
+import React, { Component } from "react";
+import Home from "./home/Home";
+import AnimalCard from "./animal/AnimalCard";
+import LocationCard from "./location/LocationCard";
+import EmployeeCard from "./employee/EmployeeCard";
+import OwnerCard from "./owner/OwnerCard";
+
+class ApplicationViews extends Component {
+  render() {
+    return (
+      <>
+        <Route
+          exact
+          path="/"
+          render={props => {
+            return <Home />;
+          }}
+        />
+        <Route
+          path="/animals"
+          render={props => {
+            return <AnimalCard />;
+          }}
+        />
+        <Route
+          path="/locations"
+          render={props => {
+            return <LocationCard />;
+          }}
+        />
+        <Route
+          path="/employees"
+          render={props => {
+            return <EmployeeCard />;
+          }}
+        />
+        <Route
+          path="/owners"
+          render={props => {
+            return <OwnerCard />;
+          }}
+        />
+      </>
+    );
+  }
+}
+
+export default ApplicationViews;
